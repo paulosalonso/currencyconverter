64c106f02e02fe57aef00ccda4427f972bfe1733
Paulo Alonso <paulo.alonso@luizalabs.com>
2022-01-26 13:29:42 -0300

    Project start

---
+++ src/test/java/com/github/paulosalonso/currencyconverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paulosalonso.currencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
