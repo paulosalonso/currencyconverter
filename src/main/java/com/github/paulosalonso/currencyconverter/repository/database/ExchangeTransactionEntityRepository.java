53b2df87a99cd2d641271016d7bc82f4896fad5f
Paulo Alonso <paulo.alonso@luizalabs.com>
2022-01-30 18:13:46 -0300

    It removes local map method and uses dedicated mapper class to transform dto in model instance

--- src/main/java/com/github/paulosalonso/currencyconverter/repository/database/ExchangeTransactionEntityRepository.java
+++ src/main/java/com/github/paulosalonso/currencyconverter/repository/database/ExchangeTransactionEntityRepository.java
@@ -2,6 +2,10 @@ package com.github.paulosalonso.currencyconverter.repository.database;
 
 import com.github.paulosalonso.currencyconverter.repository.database.entity.ExchangeTransactionEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface ExchangeTransactionEntityRepository
-    extends ReactiveCrudRepository<ExchangeTransactionEntity, String> {}
+    extends ReactiveCrudRepository<ExchangeTransactionEntity, String> {
+
+  Flux<ExchangeTransactionEntity> findAllByUserId(String userId);
+}
